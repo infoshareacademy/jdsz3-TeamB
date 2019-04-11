6b80feb0c59b704f83d6841271a049830c60653d
PatrykKerlin <kermitgda@gmail.com>
2019-04-11 20:34:38 +0200

    dodano nowe zapytania

--- projektsql/patryk_kerlin/rekompensaty.sql
+++ projektsql/patryk_kerlin/rekompensaty.sql
@@ -92,7 +92,76 @@ select jezyk, count(1) as liczba_wnioskow,
        count(case
          when stan_wniosku not like 'wyplacony' then id
          end
-       ) as wnioski_niewyplacone
+       ) as wnioski_niewyplacone,
+       round((100.0*count(case
+         when stan_wniosku not like 'wyplacony' then id
+         end
+       ))/count(1),2) as procent_wnioskow_niewyplaconych
 from wnioski
 group by 1
-having count(1) > 10
+having count(1) > 10;
+
+with sredni_czas as (
+  select w.jezyk,
+         avg(date_part('day', sr.data_otrzymania - w.data_utworzenia))::numeric as sredni_czas_oczekiwania_na_wyplate_rekompensaty
+  from wnioski w
+         full join rekompensaty r on w.id = r.id_wniosku
+         full join szczegoly_rekompensat sr on r.id = sr.id_rekompensaty
+  where stan_wniosku like 'wypl%'
+    and date_part('day', sr.data_otrzymania - w.data_utworzenia) > 0
+  group by 1
+)
+select jezyk, round(sredni_czas_oczekiwania_na_wyplate_rekompensaty,0) as sredni_czas_oczekiwania_na_wyplate_rekompensaty_w_dniach
+from sredni_czas
+
+with rozne_wnioski as (
+       select jezyk,
+              count(1) as liczba_wnioskow,
+              count(case
+                      when kwota_rekompensaty <> kwota_rekompensaty_oryginalna then id
+                     end) as liczba_roznych_wnioskow,
+              round(100 * count(case
+                                 when kwota_rekompensaty <> kwota_rekompensaty_oryginalna then id
+                     end) / count(1)::numeric, 2) as proc_rozniacych_sie_wniosków,
+              kwota_rekompensaty_oryginalna-kwota_rekompensaty as roznica
+       from wnioski
+       where lower(stan_wniosku) like 'wyp%'
+       group by 1, 5
+)
+select *
+from rozne_wnioski
+where proc_rozniacych_sie_wniosków > 0
+order by 4 desc;
+
+select jezyk, round(avg((kwota_rekompensaty_oryginalna/liczba_pasazerow)-(kwota_rekompensaty/liczba_pasazerow)),2)
+from wnioski
+where kwota_rekompensaty_oryginalna-kwota_rekompensaty > 0
+group by 1
+
+
+select stan_wniosku, count(1)
+from wnioski
+where jezyk like 'pl'
+group by 1
+
+with wnioski_wszystkie as (
+  select jezyk, count(1) as liczba_wszystkich_wnioskow
+  from wnioski
+  group by 1
+),
+     wnioski_niewyplacone as (
+       select jezyk, stan_wniosku, count(1) as liczba_wnioskow
+       from wnioski
+       where stan_wniosku not in ('wyplacony', 'zamkniety', 'nowy', 'akcja sadowa',
+                                  'wyslany do operatora', 'analiza zaakceptowana',
+                                  'wygrany w sadzie', 'zaakceptowany przez operatora')
+       group by 1, 2
+       order by 1, 2
+     )
+select wn.stan_wniosku, wn.jezyk, wn.liczba_wnioskow,
+       round(100 * ((wn.liczba_wnioskow)::numeric / ww.liczba_wszystkich_wnioskow),2) as procent_wnioskow
+from wnioski_niewyplacone wn
+full join wnioski_wszystkie ww on wn.jezyk = ww.jezyk
+where wn.jezyk is not null
+and wn.liczba_wnioskow > 20
+order by 1, 2
