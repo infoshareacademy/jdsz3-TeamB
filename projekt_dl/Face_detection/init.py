9649341f892ba69c1f0e586f13958559daf31b59
Patryk Kerlin <patryk.kerlin@gmail.com>
2019-09-14 16:33:54 +0200

    added face detection

--- projekt_dl/Face_detection/init.py
+++ projekt_dl/Face_detection/init.py
@@ -1,24 +1,29 @@
 import cv2 as cv
 import time
 from imutils.video import FPS
+import urllib
 import numpy as np
 
 # Load the model.
-net = cv.dnn.readNet('CIFAR_Model_v2_RESNET.xml',
-                     'CIFAR_Model_v2_RESNET.bin')
+net = cv.dnn.readNet('face-detection-adas-0001.xml',
+                     'face-detection-adas-0001.bin')
 # Specify target device.
-#net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+#net.setPreferableTarget(cv.dnn.DNN_TARGET_MYRIAD)
 # Read an image.
-camera = cv.VideoCapture(0)
+#camera = cv.VideoCapture(0)
 #camera.resolution = (640, 480)
 #camera.framerate = 16
 #rawCapture = cv.PiRGBArray(camera, size=(640, 480))
 
-time.sleep(0.1)
+#time.sleep(0.1)
 fps = FPS().start()
 
 while True:
-    ret, image = camera.read()
+    imgResp = urllib.request.urlopen('http://10.58.74.150:8080/shot.jpg')
+    imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+    camera = cv.imdecode(imgNp, -1)
+    image = camera
+    #ret, image = camera.read()
 #    image = frame.array
     
 # frame = cv.imread('/home/pi/Pictures/street_and_person.jpg')
@@ -26,21 +31,20 @@ while True:
 #     raise Exception('Image not found!')
 
 # Prepare input blob and perform an inference.
-    blob = cv.dnn.blobFromImage(image, size=(640, 480), ddepth=cv.CV_8U)
+    blob = cv.dnn.blobFromImage(image, ddepth=cv.CV_8U)
     net.setInput(blob)
     out = net.forward()
-    print(np.argmax(out, axis=0))
+    #print(out.shape)
 # Draw detected faces on the frame.
-    for detection in out:
-        # print(detection)
+    for detection in out.reshape(-1, 7):
+        #print(detection)
         confidence = float(detection[2])
-        # print(confidence)
         xmin = int(detection[3] * image.shape[1])
         ymin = int(detection[4] * image.shape[0])
         xmax = int(detection[5] * image.shape[1])
         ymax = int(detection[6] * image.shape[0])
         if confidence > 0.5:
-            print(confidence)
+            #print(confidence)
             cv.rectangle(image, (xmin, ymin), (xmax, ymax), color=(0, 255, 0))
             cv.putText(image, str(confidence), (xmin, ymin), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
     cv.imshow("Frame", image)
