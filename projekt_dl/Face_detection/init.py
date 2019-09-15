af9d3c5dd4c3102c42d7944670c2e4bb3f9a5d50
Patryk Kerlin <patryk.kerlin@gmail.com>
2019-09-15 11:37:49 +0200

    cleaned and tuned init.py for face detection

--- projekt_dl/Face_detection/init.py
+++ projekt_dl/Face_detection/init.py
@@ -3,59 +3,68 @@ import time
 from imutils.video import FPS
 import urllib
 import numpy as np
+import time
+import statistics
+
+fpsList = []
+
+# Measure elapsed time
+s = time.time()
 
 # Load the model.
 net = cv.dnn.readNet('face-detection-adas-0001.xml',
                      'face-detection-adas-0001.bin')
 # Specify target device.
-#net.setPreferableTarget(cv.dnn.DNN_TARGET_MYRIAD)
-# Read an image.
-#camera = cv.VideoCapture(0)
-#camera.resolution = (640, 480)
-#camera.framerate = 16
-#rawCapture = cv.PiRGBArray(camera, size=(640, 480))
+net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
 
-#time.sleep(0.1)
+# Get IP adress of camera
+ipAdress = str(input('Wprowadź adres IP kamery: '))
+
+# Count FPS
 fps = FPS().start()
 
 while True:
-    imgResp = urllib.request.urlopen('http://10.58.74.150:8080/shot.jpg')
-    imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
-    camera = cv.imdecode(imgNp, -1)
-    image = camera
-    #ret, image = camera.read()
-#    image = frame.array
-    
-# frame = cv.imread('/home/pi/Pictures/street_and_person.jpg')
-# if frame is None:
-#     raise Exception('Image not found!')
+    try:
+        # Read an image.
+        imgResp = urllib.request.urlopen('http://'+ipAdress+':8080/shot.jpg')
+        imgNp = np.array(bytearray(imgResp.read()), dtype=np.uint8)
+        image = cv.imdecode(imgNp, -1)
+    except:
+        print('Connection lost...')
+        break
 
 # Prepare input blob and perform an inference.
     blob = cv.dnn.blobFromImage(image, ddepth=cv.CV_8U)
     net.setInput(blob)
     out = net.forward()
-    #print(out.shape)
+
 # Draw detected faces on the frame.
     for detection in out.reshape(-1, 7):
-        #print(detection)
         confidence = float(detection[2])
         xmin = int(detection[3] * image.shape[1])
         ymin = int(detection[4] * image.shape[0])
         xmax = int(detection[5] * image.shape[1])
         ymax = int(detection[6] * image.shape[0])
         if confidence > 0.5:
-            #print(confidence)
-            cv.rectangle(image, (xmin, ymin), (xmax, ymax), color=(0, 255, 0))
-            cv.putText(image, str(confidence), (xmin, ymin), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-    cv.imshow("Frame", image)
-    key = cv.waitKey(1) & 0xFF
-    #rawCapture.truncate(0)
+            percentage = str(int(confidence*100))
+            cv.rectangle(image, (xmin, ymin), (xmax, ymax), color=(0, 255, 0), thickness=2)
+            cv.putText(image, percentage, (xmin, (ymin - 5)), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            if len(percentage) == 2:
+                cv.putText(image, '%', ((xmin + 40), (ymin - 5)), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            elif len(percentage) == 3:
+                cv.putText(image, '%', ((xmin + 60), (ymin - 5)), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     fps.update()
-            
+    fps.stop()
+    fpsList.append(round(fps.fps(), 2))
+    fpsShow = str(round(fps.fps(), 2))
+    cv.putText(image, fpsShow, (520, 25), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0))
+    cv.putText(image, 'FPS', (590, 25), cv.FONT_HERSHEY_SIMPLEX, 0.75, (0, 255, 0))
+    cv.putText(image, '''Press 'Q' to exit...''', (10, 470), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+    cv.imshow('Frame', image)
+    key = cv.waitKey(1) & 0xFF
+
     if key == ord("q"):
         break
-        
-fps.stop()
-print('FPS:', fps.fps())
-# Save the frame to an image file.
-# cv.imwrite('out.png', frame)
+
+print('[INFO]', round(statistics.mean(fpsList), 2), 'average FPS')
+print('[INFO]', int(time.time() - s), 'seconds elapsed')
---
+++ projekt_dl/readme.md
@@ -0,0 +1,36 @@
+# **Projekt Machine Learning**
+&nbsp;
+&nbsp;
+## Porównanie modeli oraz predykcja zdolności kredytowej na podstawie wybranej bazy danych
+&nbsp;
+&nbsp;
+#### Baza danych:
+##### [Credit.csv from package ‘ISLR’](http://faculty.marshall.usc.edu/gareth-james/ISL/Credit.csv)
+&nbsp;
+&nbsp;
+## Aplikacja:
+### Kalkulator zdolności kredytowej
+##### Wymagania:
+- środowisko python (python.exe dodany do PATH)
+- NumPy, XGBoost oraz SciKit-Learn
+
+&nbsp;
+&nbsp;
+#### DEFINITION OF DONE
+- przygotowanie i oczyszczenie danych do analizy,
+- założenie repozytorium oraz branchów na githubie
+- analiza bazy danych za pomocą odpowiednich skryptów
+- porównanie modeli oraz wybór najlepszego
+- przygotowanie aplikacji
+- sformułowanie wniosków
+- umieszczenie skryptów w repozytorium
+- wizualizacja wnioskow
+
+&nbsp;
+&nbsp;
+&nbsp;
+##### Osoby winne popełnienia owego projektu:
+- Rafał Kazubowski
+- Mateusz Kozak
+- Patryk Kerlin
+- Rafał Wensław
